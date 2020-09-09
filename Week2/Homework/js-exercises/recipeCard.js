4c945ec0b351e9ca4f70615457ccc563700f9e1d
Vaida <93vaida@gmail.com>
2020-09-09 22:30:30 +0200

    using for... loop to log object properties

---
+++ Week2/Homework/js-exercises/recipeCard.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//Declare a variable that holds an empty object literal(your meal recipe).
+let rotiRecipe = {};
+
+// Give the object 3 properties: a title(string), a servings(number) and an ingredients(array of strings) property.
+(rotiRecipe.Title = "Roti"),
+  (rotiRecipe.Servings = 4),
+  (rotiRecipe.Ingridients = [
+    "700gr chicken thighs - skinless, bonless",
+    "400gr green beans",
+    "500gr potatoes",
+    "4 eggs",
+    "3 madame jeanette peppers",
+    "4 roti breads",
+  ]);
+
+// Log each property out separately, using a loop(for, while or do /while)
+
+for (let key in rotiRecipe) {
+  if (rotiRecipe.hasOwnProperty(key)) {
+    console.log(key + ": " + rotiRecipe[key]);
+  }
+}
