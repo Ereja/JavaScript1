0d4eb953ddb2801c7c5b6b9ae1eeb2f2662125cc
Vaida <93vaida@gmail.com>
2020-09-09 22:33:37 +0200

    added comments

--- Week2/Homework/js-exercises/commaRemoval.js
+++ Week2/Homework/js-exercises/commaRemoval.js
@@ -1,6 +1,12 @@
 "use strict";
-
+//Add the variable to your file.
 let myString = "hello,this,is,a,difficult,to,read,sentence";
+
+//Log the length of myString.
 console.log(myString.length); // prints 42
+
+//Find a way to remove the commas from the string and replace them with spaces
 myString = myString.replace(/,/g, " "); // g stands for global and will remove all the commas in a string.
+
+//After replacing the commas, log myString to see if you succeeded.
 console.log(myString); // hello this is a difficult to read sentence
--- Week2/Homework/js-exercises/evenOddReporter.js
+++ Week2/Homework/js-exercises/evenOddReporter.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//Create a for loop, that iterates from 0 to 20.
 for (let i = 0; i <= 20; i++) {
   //only numbers whose remainder isn`t 0 are odd
   if (i % 2 != 0) {
@@ -8,3 +8,6 @@ for (let i = 0; i <= 20; i++) {
     console.log("The number", +i, "is even!");
   }
 }
+
+//If it's odd, log to the console The number [PUT_NUMBER_HERE] is //odd!.
+//If it's even, log to the console The number [PUT_NUMBER_HERE] is //even!.
