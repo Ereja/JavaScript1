502c8854f3eb40ed5453002c196970f99163a57d
Vaida <93vaida@gmail.com>
2020-09-17 16:16:07 +0200

    dog age converter

---
+++ Week3/Homework/JS-exercises/2dogYears.js
@@ -0,0 +1,22 @@
+/*
+Write a function named calculateDogAge.
+It takes 1 argument: your (fictional) puppy's age (number).
+Calculate your dog's age based on the conversion rate of 1 human year to 7 dog years.
+Return a string: "Your doggie is [CALCULATED_VALUE] years old in dog years!"
+Call the function three times with different sets of values.
+*/
+
+"use strict"
+
+function calculateDogAge(number) {
+    const dogAge = Math.round(number) * 7;
+    return `Your doggie is ${dogAge} years old in dog years!`;
+}
+
+//call function with different values
+calculateDogAge(2);
+calculateDogAge(5.5);
+calculateDogAge(7);
+
+//console log to see if function is working
+console.log(calculateDogAge(2.5));
