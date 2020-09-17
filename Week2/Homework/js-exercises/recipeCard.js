8ba8c396bf7bd0cabd087ef136dce8a56416e5bc
Vaida <93vaida@gmail.com>
2020-09-17 15:09:40 +0200

    fixed formatting

--- Week2/Homework/js-exercises/recipeCard.js
+++ Week2/Homework/js-exercises/recipeCard.js
@@ -5,8 +5,8 @@ let rotiRecipe = {};
 
 // Give the object 3 properties: a title(string), a servings(number) and an ingredients(array of strings) property.
 (rotiRecipe.Title = "Roti"),
-  (rotiRecipe.Servings = 4),
-  (rotiRecipe.Ingridients = [
+(rotiRecipe.Servings = 4),
+(rotiRecipe.Ingridients = [
     "700gr chicken thighs - skinless, bonless",
     "400gr green beans",
     "500gr potatoes",
