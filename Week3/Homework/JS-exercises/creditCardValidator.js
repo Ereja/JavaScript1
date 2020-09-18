ad6d3f0b7fca7c6d38feb8c967ad2a116425d5f8
Vaida <93vaida@gmail.com>
2020-09-18 15:48:28 +0200

    function that validates credit card number if it matches certain traits

---
+++ Week3/Homework/JS-exercises/creditCardValidator.js
@@ -0,0 +1,62 @@
+/*
+Input must be 16 characters
+All characters must be numbers
+At least two different numbers should be represented
+The last number must be even
+The sum of all the numbers must be greater than 16
+*/
+
+"use strict";
+
+function creditCardValidator(cardNumber) {
+  //checking if there are 16 numbers
+  if (cardNumber.length !== 16) {
+    return `The input (${cardNumber}) must contain 16 numbers!`;
+    //checking if everything is a number
+  } else if (isNaN(cardNumber)) {
+    return `The input (${cardNumber}) must contain only numbers!`;
+    //including function for different numbers
+  } else if (differentNumbers(cardNumber)) {
+    return `The input (${cardNumber}) must contain more than one type of number!`;
+    //including function of numbers num to check if sum is greater than 16
+  } else if (numbersValue(cardNumber) <= 16) {
+    return `The sum of input (${cardNumber}) must be greater than 16!`;
+    //only if last number is even, the whole number will be even. We can use remainder to check it
+  } else if (cardNumber % 2 !== 0) {
+    return `The last number of input (${cardNumber}) must be even!`;
+  } else {
+    return `${cardNumber} is a valid credit card number!`;
+  }
+}
+
+//function to check if there is more than one type of number
+function differentNumbers(number) {
+  for (let i = 0; i < number.length; i++) {
+    if (number[0] !== number[i]) return false;
+  }
+  return true;
+}
+
+//function to add all numbers
+function numbersValue(number) {
+  let sum = 0;
+  while (number) {
+    sum += number % 10;
+    number = Math.floor(number / 10);
+  }
+  return sum;
+}
+
+creditCardValidator("4444444444444444");
+creditCardValidator("a92332119c011112");
+creditCardValidator("1111111111111110");
+creditCardValidator("6666666666666661");
+creditCardValidator("6666666666661666");
+creditCardValidator("9999777788880000");
+
+console.log(creditCardValidator("4444444444444444"));
+console.log(creditCardValidator("a92332119c011112"));
+console.log(creditCardValidator("1111111111111110"));
+console.log(creditCardValidator("6666666666666661"));
+console.log(creditCardValidator("6666666666661666"));
+console.log(creditCardValidator("9999777788880000"));
