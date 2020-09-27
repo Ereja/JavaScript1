4f1254c673133a4528a789823dfd9f8fbd735833
Vaida <93vaida@gmail.com>
2020-09-27 11:24:23 +0200

    removed unnecessary plus sign

--- Week2/Homework/js-exercises/evenOddReporter.js
+++ Week2/Homework/js-exercises/evenOddReporter.js
@@ -3,9 +3,9 @@
 for (let i = 0; i <= 20; i++) {
   //only numbers whose remainder isn`t 0 are odd
   if (i % 2 != 0) {
-    console.log("The number", +i, "is odd!");
+    console.log("The number", i, "is odd!");
   } else {
-    console.log("The number", +i, "is even!");
+    console.log("The number", i, "is even!");
   }
 }
 
