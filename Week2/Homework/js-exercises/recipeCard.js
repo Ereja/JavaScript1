5d80db8665231bf97c39056fa47611b6135f0db6
Vaida <93vaida@gmail.com>
2020-09-27 15:00:26 +0200

    removed the unnecessary parentheses

--- Week2/Homework/js-exercises/recipeCard.js
+++ Week2/Homework/js-exercises/recipeCard.js
@@ -4,16 +4,16 @@
 let rotiRecipe = {};
 
 // Give the object 3 properties: a title(string), a servings(number) and an ingredients(array of strings) property.
-(rotiRecipe.Title = "Roti"),
-(rotiRecipe.Servings = 4),
-(rotiRecipe.Ingridients = [
-    "700gr chicken thighs - skinless, bonless",
-    "400gr green beans",
-    "500gr potatoes",
-    "4 eggs",
-    "3 madame jeanette peppers",
-    "4 roti breads",
-  ]);
+rotiRecipe.Title = "Roti";
+rotiRecipe.Servings = 4;
+rotiRecipe.Ingridients = [
+  "700gr chicken thighs - skinless, bonless",
+  "400gr green beans",
+  "500gr potatoes",
+  "4 eggs",
+  "3 madame jeanette peppers",
+  "4 roti breads",
+];
 
 // Log each property out separately, using a loop(for, while or do /while)
 
@@ -23,6 +23,6 @@ for (let prop in rotiRecipe) {
   }
 }
 
-//for (let prop in rotiRecipe) {
+// for (let prop in rotiRecipe) {
 //    console.log(prop, ":", rotiRecipe[prop]);
-//}
+// }
