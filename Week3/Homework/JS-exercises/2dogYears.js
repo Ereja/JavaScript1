484864489d358b842e62d6fc6607d97091a448c3
Vaida <93vaida@gmail.com>
2020-09-27 16:00:23 +0200

    changed round method to floor

--- Week3/Homework/JS-exercises/2dogYears.js
+++ Week3/Homework/JS-exercises/2dogYears.js
@@ -6,15 +6,14 @@ Return a string: "Your doggie is [CALCULATED_VALUE] years old in dog years!"
 Call the function three times with different sets of values.
 */
 
-"use strict"
+"use strict";
 
 function calculateDogAge(number) {
-    const dogAge = Math.round(number) * 7;
-    return `Your doggie is ${dogAge} years old in dog years!`;
+  const dogAge = Math.floor(number) * 7; //some people give age of their animals in presicion of months but it would make no sense to say that an actual human is 24 years and six months old!
+  return `Your doggie is ${dogAge} years old in dog years!`;
 }
 
 //call function with different values
 calculateDogAge(2);
 calculateDogAge(5.5);
 calculateDogAge(7);
-
